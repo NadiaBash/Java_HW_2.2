3c53015296f6b56ef2cab983dc69501eb0223d17
Nadia Bash <pretty_belka_2018@bk.ru>
2022-05-21 20:15:33 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int balance = 100;
+        int amount = 1100;
+        int limit = 1;
+
+        int bonus = amount / 100 * limit;
+
+        if (amount < 1000) {
+            bonus = 0;
+            if (amount >= 1000)
+                bonus = amount / 100 * limit;
+        }
+
+        int total = balance + bonus;
+
+        System.out.println(total = amount + balance + bonus);
+
+
+    }
+}
+
